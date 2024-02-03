8c4f77051af95626819d34a4c5dd7095e1ddb79a
Trevor Wilson <trevortwilson@hotmail.com>
2024-02-03 07:16:21 -0700

    Initial Commit

---
+++ RewriteVersion4/RewriteVersion4App.swift
@@ -0,0 +1,17 @@
+//
+//  RewriteVersion4App.swift
+//  RewriteVersion4
+//
+//  Created by trevor wilson on 2024-02-03.
+//
+
+import SwiftUI
+
+@main
+struct RewriteVersion4App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
