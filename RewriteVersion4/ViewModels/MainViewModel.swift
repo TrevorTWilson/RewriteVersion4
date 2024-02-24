f437a9446dfe4acccb64452469ffdf98cc21802c
Trevor Wilson <trevortwilson@hotmail.com>
2024-02-24 06:45:24 -0700

    fixed the list update funtion for adding a newWeldNumber

--- RewriteVersion4/ViewModels/MainViewModel.swift
+++ RewriteVersion4/ViewModels/MainViewModel.swift
@@ -12,11 +12,11 @@ class MainViewModel: ObservableObject, Equatable {
     static func == (lhs: MainViewModel, rhs: MainViewModel) -> Bool {
         // Implement custom logic to compare two MainViewModel instances for equality
         return lhs.weldingInspector == rhs.weldingInspector &&
-               lhs.selectedJob == rhs.selectedJob &&
-               lhs.selectedWeldingProcedure == rhs.selectedWeldingProcedure &&
-               lhs.selectedWelder == rhs.selectedWelder &&
-               lhs.selectedWeldNumber == rhs.selectedWeldNumber &&
-               lhs.selectedWeldPass == rhs.selectedWeldPass
+        lhs.selectedJob == rhs.selectedJob &&
+        lhs.selectedWeldingProcedure == rhs.selectedWeldingProcedure &&
+        lhs.selectedWelder == rhs.selectedWelder &&
+        lhs.selectedWeldNumber == rhs.selectedWeldNumber &&
+        lhs.selectedWeldPass == rhs.selectedWeldPass
     }
     
     // Setup for Objects to be available through scope of app
@@ -30,7 +30,7 @@ class MainViewModel: ObservableObject, Equatable {
             // You can add more logic or actions here based on the updated weldingInspector
         }
     }
-
+    
     @Published var selectedJob: WeldingInspector.Job? {
         didSet {
             // Perform actions when selectedJob is set or changed
@@ -44,7 +44,7 @@ class MainViewModel: ObservableObject, Equatable {
             // You can add more logic or actions here based on the new selectedJob value
         }
     }
-
+    
     @Published var selectedWeldingProcedure: WeldingInspector.Job.WeldingProcedure?{
         didSet {
             // Perform actions when selectedJob is set or changed
@@ -84,7 +84,7 @@ class MainViewModel: ObservableObject, Equatable {
             // You can add more logic or actions here based on the new selectedJob value
         }
     }
-
+    
     @Published var selectedWeldPass: WeldingInspector.Job.WeldingProcedure.Welder.WeldNumbers.Parameters?{
         didSet {
             // Perform actions when selectedJob is set or changed
@@ -109,7 +109,7 @@ class MainViewModel: ObservableObject, Equatable {
     func setSelectedJob(job: WeldingInspector.Job) {
         selectedJob = job
     }
-
+    
     
     // Methods to store and retrieve weldingInspector data from local JSON file (default 'Seed' data available if no file present
     func loadWeldingInspector() {
@@ -145,75 +145,89 @@ class MainViewModel: ObservableObject, Equatable {
     
     func addProcedure(name: String, type: String = "", usage: String = "", owner: String = "", minRanges: [String: CGFloat] = [:], maxRanges: [String: CGFloat] = [:], weldersQualified: [WeldingInspector.Job.WeldingProcedure.Welder] = []) {
         guard let jobIndex = weldingInspector.jobs.firstIndex(where: { $0.id == selectedJob?.id }),
-            var updatedJob = selectedJob
+              var updatedJob = selectedJob
         else {
             return
         }
-
+        
         let newProcedure = WeldingInspector.Job.WeldingProcedure(name: name, type: type, usage: usage, owner: owner, minRanges: minRanges, maxRanges: maxRanges, weldersQualified: weldersQualified)
-
+        
         updatedJob.weldingProcedures.append(newProcedure)
-
+        
         weldingInspector.jobs[jobIndex] = updatedJob
         
         
-        //print(jobIndex)
-
+        print(jobIndex)
+        
     }
-
-
+    
+    
     func addWelder(name: String, welderId: String = "", pressureNumber: String = "", pressureExpiry: String = "", welds: [WeldingInspector.Job.WeldingProcedure.Welder.WeldNumbers] = []) {
         guard var updatedProcedure = selectedWeldingProcedure else {
             return
         }
-
+        
         let newWelder = WeldingInspector.Job.WeldingProcedure.Welder(name: name, welderId: welderId, pressureNumber: pressureNumber, pressureExpiry: pressureExpiry, welds: welds)
-
+        
         updatedProcedure.weldersQualified.append(newWelder)
-
+        
         if let jobIndex = weldingInspector.jobs.firstIndex(where: { $0.id == selectedJob?.id }), let procedureIndex = updatedProcedure.weldersQualified.firstIndex(where: { $0.id == newWelder.id }) {
             weldingInspector.jobs[jobIndex].weldingProcedures[procedureIndex] = updatedProcedure
             
-            //print("\(jobIndex)  \(procedureIndex)")
+            print("\(jobIndex)  \(procedureIndex)")
         }
     }
-
-
+    
+    
     func addWeldNumber(name: String, parametersCollected: [WeldingInspector.Job.WeldingProcedure.Welder.WeldNumbers.Parameters] = []) {
         guard var updatedWelder = selectedWelder else {
+            print("Failed")
             return
         }
-
+        
+        print("step one")
         let newWeldNumber = WeldingInspector.Job.WeldingProcedure.Welder.WeldNumbers(name: name, parametersCollected: parametersCollected)
-
+        
         updatedWelder.welds.append(newWeldNumber)
-
-        if let jobIndex = weldingInspector.jobs.firstIndex(where: { $0.id == selectedJob?.id }), let procedureIndex = selectedJob?.weldingProcedures.firstIndex(where: { $0.id == selectedWeldingProcedure?.id }), let welderIndex = selectedWeldingProcedure?.weldersQualified.firstIndex(where: { $0.id == selectedWelder?.id }) {
+        print("Appended temp")
+        
+        
+        if let jobIndex = weldingInspector.jobs.firstIndex(where: { $0.id == selectedJob?.id }),
+           let welderIndex = selectedWeldingProcedure?.weldersQualified.firstIndex(where: { $0.id == selectedWelder?.id }),
+           let procedureIndex = selectedJob?.weldingProcedures.firstIndex(where: { $0.id == selectedWeldingProcedure?.id }){
+            
+            
+            
+            
             
             weldingInspector.jobs[jobIndex].weldingProcedures[procedureIndex].weldersQualified[welderIndex] = updatedWelder
+            
+            print("\(jobIndex)  \(procedureIndex)  \(welderIndex)")
         }
     }
-
-
+    
+    
     func addParameters(passName: String, collectedValues: [String: CGFloat] = [:]) {
         guard var updatedWeldNumber = selectedWeldNumber else {
             return
         }
-
+        
         let newParameters = WeldingInspector.Job.WeldingProcedure.Welder.WeldNumbers.Parameters(passName: passName, collectedValues: collectedValues)
-
+        
         updatedWeldNumber.parametersCollected.append(newParameters)
-
+        
         if let jobIndex = weldingInspector.jobs.firstIndex(where: { $0.id == selectedJob?.id }), let procedureIndex = selectedJob?.weldingProcedures.firstIndex(where: { $0.id == selectedWeldingProcedure?.id }), let welderIndex = selectedWeldingProcedure?.weldersQualified.firstIndex(where: { $0.id == selectedWelder?.id }), let weldNumberIndex = selectedWelder?.welds.firstIndex(where: { $0.id == selectedWeldNumber?.id }) {
             
             //selectedWelder?.welds[weldNumberIndex] = updatedWeldNumber
             weldingInspector.jobs[jobIndex].weldingProcedures[procedureIndex].weldersQualified[welderIndex].welds[weldNumberIndex] = updatedWeldNumber
+            
+            print("\(jobIndex)  \(procedureIndex)  \(welderIndex)  \(weldNumberIndex)")
         }
     }
-
-
-
-
+    
+    
+    
+    
 }
 
 
